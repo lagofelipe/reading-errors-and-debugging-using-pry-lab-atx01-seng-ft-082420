43b02ac4f0ca23524fd9840015aa4ce363d6c0b0
Felipe Dolago <lagofelipe@gmail.com>
2020-07-24 16:55:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,11 +11,11 @@ def state_log(star_date)
 
 end
 
-def engage
+def engage (star_date)
 #Captain's Log, star date
+print "Captain's Log, star date #{star_date}."
 
 
-def state_log(date)
 
   #date = generate_star_date
 
